7251fae187ab8a99cb8e769a7a9ee258ca21f973
Gabriel keith <gabe.keith@yahoo.com>
2024-03-30 11:12:19 -0400

    added pico css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,65 +1,38 @@
-<header style="border-bottom: 1px solid #e8e8e8">
-    <nav aria-label="Main" lang="en" class="mainnav container">
+<script lang="ts">
+    import "@picocss/pico/css/pico.pumpkin.min.css";
+</script>
+
+<main class="container">
+    <!-- <header style="border-bottom: 1px solid #e8e8e8">
+        <nav aria-label="Main" lang="en">
+            <ul>
+                <li>
+                    <a lang="en" href="/about"><span>About</span></a>
+                </li>
+                <li>
+                    <a lang="en" href="/projects"><span>Projects</span></a>
+                </li>
+                <li>
+                    <a lang="en" href="/blog"><span>Blog</span></a>
+                </li>
+                <li>
+                    <a lang="en" href="/music"><span>Music</span></a>
+                </li>
+            </ul>
+        </nav>
+    </header> -->
+    <nav>
         <ul>
-            <li>
-                <a lang="en" href="/about"><span>About</span></a>
-            </li>
-            <li>
-                <a lang="en" href="/projects"><span>Projects</span></a>
-            </li>
-            <li>
-                <a lang="en" href="/blog"><span>Blog</span></a>
-            </li>
-            <li>
-                <a lang="en" href="/music"><span>Music</span></a>
-            </li>
+          <li><strong>Gabriel Keith</strong></li>
         </ul>
-    </nav>
-</header>
+        <ul>
+          <li><a href="/about">About</a></li>
+          <li><a href="/blog">Blog</a></li>
+          <li><a href="/projects">Projects</a></li>
+          <li><a href="/music">Music</a></li>
+        </ul>
+      </nav>
 
-<slot></slot>
+    <slot></slot>
+</main>
 
-<style>
-:global(body) {
-    margin: 0;
-    font-family: "Open Sans";
-    font-size: 22px;
-
-    background: #fdfdfd;
-}
-
-.container {
-    width: 800px;
-    margin: 0 auto;
-}
-
-
-.mainnav ul {
-    display: flex;
-    width: 100%;
-    max-width: 500px;
-    margin: 0 auto;
-    padding: 0;
-    list-style-type: none;
-}
-
-.mainnav li {
-    flex-basis: 50%;
-    flex-grow: 1;
-    flex-shrink: 1;
-}
-
-.mainnav a {
-    color: #424242;
-    text-decoration: none;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    min-height: 44px;
-}
-
-.mainnav li:hover {
-    text-decoration: underline;
-}
-</style>
